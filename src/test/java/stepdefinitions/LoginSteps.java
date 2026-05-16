90c4ac011c8d0a10f73da46d9061bdefa839dcfe
D.Chandra <chandrikadamera09@gmail.com>
2026-05-16 23:58:33 +0530

    Fixed LoginSteps constructor issue

--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -1,24 +1,27 @@
 package stepdefinitions;
 
+import factory.DriverFactory;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
-import pages.HomePage;
 import pages.LoginPage;
 
 public class LoginSteps {
 
-    HomePage homePage = new HomePage();
-    LoginPage loginPage = new LoginPage();
+    LoginPage loginPage =
+            new LoginPage(DriverFactory.getDriver());
 
-    @Given("user clicks on login")
-    public void user_clicks_on_login() {
+    @Given("user is on login page")
+    public void user_is_on_login_page() {
 
-        homePage.clickLogin();
+        DriverFactory.getDriver()
+                .get("https://example.com/login");
     }
 
-    @When("user enters username {string} and password {string}")
-    public void user_enters_username_and_password(String username, String password) {
+    @When("user enters username and password")
+    public void user_enters_username_and_password() {
 
-        loginPage.doLogin(username, password);
+        loginPage.doLogin(
+                "admin@test.com",
+                "admin123");
     }
 }
