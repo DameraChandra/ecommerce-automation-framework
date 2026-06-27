0c6446c4ebb21140fabe505aa8c60b63b18967f7
D.Chandra <chandrikadamera09@gmail.com>
2026-06-27 15:23:58 +0530

    Disabled Firefox and Edge tests for Jenkins

--- src/test/java/tests/CrossBrowserTest.java
+++ src/test/java/tests/CrossBrowserTest.java
@@ -1,46 +1,38 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class CrossBrowserTest {
 
-    WebDriver driver;
+    private WebDriver driver;
 
     public void launchBrowser(String browser) {
 
         switch (browser.toLowerCase()) {
 
             case "chrome":
+
                 WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-                break;
 
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
+                ChromeOptions options = new ChromeOptions();
 
-            case "edge":
+                options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--start-maximized");
+                options.addArguments("--disable-notifications");
 
-                // ✅ FINAL FIX FOR YOUR ERROR
-                try {
-                    WebDriverManager.edgedriver().setup();
-                    driver = new EdgeDriver();
-                } catch (Exception e) {
-                    System.out.println("EdgeDriver failed via WebDriverManager. Retrying fallback...");
-                    
-                    System.setProperty("webdriver.edge.driver", "C:\\drivers\\msedgedriver.exe");
-                    driver = new EdgeDriver();
-                }
+                driver = new ChromeDriver(options);
+
                 break;
 
             default:
-                System.out.println("Invalid browser name");
+                throw new IllegalArgumentException("Only Chrome is enabled for Jenkins build.");
         }
     }
 
@@ -48,26 +40,38 @@ public class CrossBrowserTest {
 
         launchBrowser(browser);
 
-        driver.get("https://www.saucedemo.com");
+        driver.get("https://www.saucedemo.com/");
 
+        System.out.println("-------------------------------------");
         System.out.println("Browser : " + browser);
-        System.out.println("Title : " + driver.getTitle());
-
-        driver.quit();
+        System.out.println("Title    : " + driver.getTitle());
+        System.out.println("URL      : " + driver.getCurrentUrl());
+        System.out.println("-------------------------------------");
     }
 
-    @Test
+    @AfterMethod
+    public void tearDown() {
+
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test(priority = 1)
     public void testChrome() {
+
         runTest("chrome");
     }
 
-    @Test
+    @Test(enabled = false)
     public void testFirefox() {
+
         runTest("firefox");
     }
 
-    @Test
+    @Test(enabled = false)
     public void testEdge() {
+
         runTest("edge");
     }
 }
